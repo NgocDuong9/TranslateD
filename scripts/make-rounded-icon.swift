831230b6c0ca1ddf9158961fef1595a8ae0a3954
NguyenNgocDuong <78304672+NguyenNgocDuongHumg@users.noreply.github.com>
2026-06-02 10:26:34 +0700

    refactor: add icon insetting and pixel-based transparency masking to rounded icon generation script

--- scripts/make-rounded-icon.swift
+++ scripts/make-rounded-icon.swift
@@ -8,7 +8,10 @@ guard CommandLine.arguments.count == 3 else {
 let inputURL = URL(fileURLWithPath: CommandLine.arguments[1])
 let outputURL = URL(fileURLWithPath: CommandLine.arguments[2])
 let size = 1024
-let cornerRadius: CGFloat = 224
+let iconSize = 900
+let iconInset = (size - iconSize) / 2
+let cornerRadius: CGFloat = 198
+let blackThreshold: UInt8 = 18
 
 guard let source = NSImage(contentsOf: inputURL) else {
     fatalError("Could not read icon at \(inputURL.path)")
@@ -19,23 +22,40 @@ guard let sourceImage = source.cgImage(forProposedRect: &proposedRect, context: 
     fatalError("Could not decode icon at \(inputURL.path)")
 }
 
+var pixels = [UInt8](repeating: 0, count: size * size * 4)
+
 guard let context = CGContext(
-    data: nil,
+    data: &pixels,
     width: size,
     height: size,
     bitsPerComponent: 8,
-    bytesPerRow: 0,
+    bytesPerRow: size * 4,
     space: CGColorSpaceCreateDeviceRGB(),
-    bitmapInfo: CGImageAlphaInfo.premultipliedLast.rawValue
+    bitmapInfo: CGImageAlphaInfo.premultipliedLast.rawValue | CGImageByteOrderInfo.order32Big.rawValue
 ) else {
     fatalError("Could not create icon render context")
 }
 
-let rect = CGRect(x: 0, y: 0, width: size, height: size)
-context.clear(rect)
-context.addPath(CGPath(roundedRect: rect, cornerWidth: cornerRadius, cornerHeight: cornerRadius, transform: nil))
+let canvasRect = CGRect(x: 0, y: 0, width: size, height: size)
+let iconRect = CGRect(x: iconInset, y: iconInset, width: iconSize, height: iconSize)
+
+context.clear(canvasRect)
+context.addPath(CGPath(roundedRect: iconRect, cornerWidth: cornerRadius, cornerHeight: cornerRadius, transform: nil))
 context.clip()
-context.draw(sourceImage, in: rect)
+context.draw(sourceImage, in: iconRect)
+
+for index in stride(from: 0, to: pixels.count, by: 4) {
+    let red = pixels[index]
+    let green = pixels[index + 1]
+    let blue = pixels[index + 2]
+
+    if red <= blackThreshold && green <= blackThreshold && blue <= blackThreshold {
+        pixels[index] = 0
+        pixels[index + 1] = 0
+        pixels[index + 2] = 0
+        pixels[index + 3] = 0
+    }
+}
 
 guard let outputImage = context.makeImage() else {
     fatalError("Could not render icon image")
